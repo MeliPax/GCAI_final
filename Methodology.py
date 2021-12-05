b187d464a7b59a07c0a5225deb8a702d21c3c485
Melipax <pmfura@andrew.cmu.edu>
2021-12-05 22:28:41 +0200

    added requirements for deployments

--- Methodology.py
+++ Methodology.py
@@ -5,7 +5,7 @@ from PIL import Image
 def chatbots():
     st.title("Methodology")
     st.markdown("** _Review of Existing Literature_** - We review existing literature on the adoption of chatbots in the banking sector of some African countries with the hope of understanding the impact and some of the challenges.")
-    lit = Image.open("assets/lit.PNG")
-    st.image(lit)
-    url4 = "https://dailypost.ng/2021/01/19/how-to-write-an-effective-literature-review/"
-    st.write("Image fom [Source](%s)" % url4)
+    # lit = Image.open("assets/lit.PNG")
+    # st.image(lit)
+    # url4 = "https://dailypost.ng/2021/01/19/how-to-write-an-effective-literature-review/"
+    # st.write("Image fom [Source](%s)" % url4)
